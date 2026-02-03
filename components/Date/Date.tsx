a1a1acbee9a73247dcfd65ca10375fae3e60daca
mahfuzhannan <mafzdeveloper@gmail.com>
2026-02-03 14:31:22 +0000

    fix: add classname option for date to support alternate view

--- components/Date/Date.tsx
+++ components/Date/Date.tsx
@@ -4,13 +4,21 @@ import {
   dtHijriNowLocaleCustomFormat,
   dtNowLocaleCustomFormat,
 } from "@/lib/datetimeUtils"
+import { cn } from "@/lib/utils"
 
-export default function Date() {
+export default function Date({
+  className = "",
+}) {
   const englishDate = dtNowLocaleCustomFormat("dddd D MMMM YYYY")
   const hijriDate = dtHijriNowLocaleCustomFormat("iD iMMMM iYYYY")
 
   return (
-    <div className="text-mosqueBrand-onPrimary text-center md:text-left">
+    <div
+      className={cn(
+        "text-mosqueBrand-onPrimary text-center md:text-left",
+        className,
+      )}
+    >
       <p className="font-bold text-xl md:text-5xl">{englishDate}</p>
       <p className="md:mt-5 text-xl md:text-4xl">{hijriDate}</p>
     </div>
